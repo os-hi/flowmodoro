a4fd7d50fddf2d011541ab24e83ed19bf0836452
Jake <110341554+os-hi@users.noreply.github.com>
2024-01-15 22:12:27 +0800

    feat: impoted timer component

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import Timer from './components/Timer.vue'
+</script>
+
+<template>
+  <div class="h-screen w-screen flex justify-center items-center">
+    <Timer/>
+  </div>
+</template>
