ad8a90fd3fba24d8b56d810edc38df52e68835b5
Jake <110341554+os-hi@users.noreply.github.com>
2024-01-18 23:05:54 +0800

    feat: added breatime component as well as some flex styling

--- src/views/Flowmodoro.vue
+++ src/views/Flowmodoro.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
+    <div class="flex flex-col gap-20">
+        <Breaktime />
         <Timer />
         <!-- <Countdown /> -->
     </div>
 </template>
 
 <script setup lang="ts">
+    import Breaktime from '../components/Breaktime.vue';
     import Timer from '../components/Timer.vue';
     // import Countdown from '../components/Countdown.vue';
 </script>
